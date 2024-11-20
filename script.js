23fa885916bb21d851e3cb2cd329d4bc4299694d
BOLUWATIFE-DAVID-ADANLA <adanlab4@gmail.com>
2024-11-20 11:13:37 +0100

    i worked on maps in js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+// maps key value pairs 
+// "username" , "boluwatife"
+
+var userdetails = new Map([
+    ['username', 'boluwatife'],
+    ['age', 23],
+    ['hobbies', 'beating up strangers']
+]);
+
+
+// userdetails.set('favourite color', 'blue');
+// console.log(userdetails);
+
+username = ['jola', 'tinubu', 'atiku'];
+
+userdetails.set('friends name', username[1]);
+userdetails.delete('friends name');
+console.log(userdetails);
